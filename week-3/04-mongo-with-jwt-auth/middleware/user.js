1a9a6add33fb6b36eeddccadeb2634d89b9ac4b2
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-15 14:55:15 +0530

    Update user.js

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,4 +1,23 @@
+const {JWT_SECRET} = require("../config");
+const jwt = require("jsonwebtoken");
+// Middleware for handling auth
+
 function userMiddleware(req, res, next) {
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+    const decodedValue = jwt.verify(jwtToken,JWT_SECRET);
+
+    if (decodedValue.username) {
+        req.username = decodedValue.username;
+        req.randomData = "Adsadsadsadssd";
+        next();
+    }
+    else{
+        res.status(403).json({
+            msg: "You are not authenticated"
+        })
+    }
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 }
